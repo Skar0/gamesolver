e1a51ae64fa9723e7056d9fe7b082b0eccf59c19
clement <clement.tamines@gmail.com>
2017-07-28 15:37:50 +0200

    Added an operation.py file to tools. This contains common behaviour and routines for our algorithms.

---
+++ tools/operations.py
@@ -0,0 +1,11 @@
+
+def opponent(j):
+    """
+    Returns the opponent of player j.
+    :param j: the player (0 or 1).
+    :return: its opponent.
+    """
+    if j == 1:
+        return 0
+    else:
+        return 1
