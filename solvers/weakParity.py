4b78aa674067fe4c1ce4a270675965b097cbfa28
clement <clement.tamines@gmail.com>
2017-07-31 21:15:17 +0200

    Moved some functions to tools package. New version is now more readable and uses the new reachability solver

--- solvers/weakParity.py
+++ solvers/weakParity.py
@@ -1,55 +1,32 @@
 from collections import defaultdict
 
-import reachability
+from solvers import reachability
+from tools import operations as ops
 
 
-def init_out(g):
-    out = defaultdict(int)
-    for node in g.get_nodes():
-        for pred in g.get_predecessors(node):
-            out[pred] += 1
-    return out
-
-
-def maxPriority(g):
-    nodes = g.nodes
-    m = max(nodes.items(), key=lambda (k, v): v[1])[1][1]
-    return m
-
-def nodesmax(g,i):
-    nodes = g.nodes
-    c = filter(lambda (k, v): v[1] == i,nodes.items())
-    l = []
-    for e in c :
-        l.append(e[0])
-    return l
-
-
-def weakParity_solver(g):
+def weak_parity_solver(g):
 
     h = g
-    i = maxPriority(h)
+    i = ops.max_priority(h) # Maximum priority in h
 
-    regions = defaultdict(lambda: -1)
+    W1 = []
+    W2 = []
     strategies = defaultdict(lambda: -1)
 
+    # Considering priorities from i to 0 in decreasing order
     for k in range(i, -1, -1):
-        (Ak,Sk) = reachability.reachability_solver(h, nodesmax(h, k), k % 2)
+        current_player = k % 2
+        (Ak, Sk), (regions_opponent, strategy_opponent) = reachability.reachability_solver_tuples(h, ops.i_priority_node(h, k), current_player)
         #print "iter "+str(k)+" -- "+str(Ak)+" -- "+str(Sk)
         p = []
-        for node in Ak:
-            if Sk[node] != -1:
-                strategies[node] = Sk[node]
-            if Ak[node] == k%2:
-                regions[node] = k%2
-            else:
-                p.append(node)
-        h = h.subgame(p)
+        strategies.update(strategy_opponent)
+        strategies.update(Sk)
 
-    return regions, strategies
+        if current_player == 0:
+            W1.extend(Ak)
+        else:
+            W2.extend(Ak)
 
-def opposite(j):
-    if j == 1:
-        return 0
-    else:
-        return 1
+        h = h.subgame(regions_opponent)
+
+    return W1, W2, strategies
