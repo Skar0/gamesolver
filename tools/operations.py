7b99792bce1ea889012de9d167178b300ddd52d6
clement <clement.tamines@gmail.com>
2017-07-31 12:03:17 +0200

    Added functions to get max priority and specific-priority nodes

--- tools/operations.py
+++ tools/operations.py
@@ -1,4 +1,3 @@
-
 def opponent(j):
     """
     Returns the opponent of player j.
@@ -9,3 +8,26 @@ def opponent(j):
         return 0
     else:
         return 1
+
+
+def i_priority_node(g, i):
+    """
+    Returns all node of priority i in game graph g.
+    :param g: the game graph.
+    :param i: the requested priority.
+    :return: a list of nodes of priority i in g.
+    """
+    nodes = g.nodes  # Nodes from g
+    # get all node_id's in node tuple (node_id, (node_player, node_priority)) when node_priority is i
+    return [k for k, v in nodes.iteritems() if v[1] == i]
+
+
+def max_priority(g):
+    """
+    Returns the maximum priority occuring in game graph g.
+    :param g: a game graph.
+    :return: the maximum priority in g.
+    """
+    nodes = g.nodes  # Nodes from g
+    # get maximum node tuple (node_id, (node_player, node_priority)) according to its priority, then select its priority
+    return max(nodes.iteritems(), key=lambda (k, v): v[1])[1][1]
