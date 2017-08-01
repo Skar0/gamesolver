77d8b177406d566620a0b814c949665cc2826f51
clement <clement.tamines@gmail.com>
2017-08-01 11:57:06 +0200

    Added more tests for strong parity. Added weak parity tests.

--- test/strongparityTest.py
+++ test/strongparityTest.py
@@ -28,3 +28,19 @@ def example_3():
     print (a == [2,1,3,4]) and b =={4:4,2:4,1: 2} and c == [6,7,5] and d == {7:6,6:6,5:6}
 
 example_3()
+
+
+def example_4():
+    g = io.load_from_file("../assets/strong parity/example_4.txt")
+    (a,b),(c,d) = sp.strongparity_solver2(g)
+    print a == [2, 1, 5] and b == {1: 2, 2: 2, 5: 5} and c ==[6, 3, 4] and d == {3: 6, 4: 3, 6: 6}
+
+example_4()
+
+
+def example_5():
+    g = io.load_from_file("../assets/strong parity/example_5.txt")
+    (a,b),(c,d) = sp.strongparity_solver2(g)
+    print a == [2, 1, 5] and b =={1: 2, 2: 2, 5: 5}and c ==[7, 6, 3, 4] and d == {3: 6, 4: 3, 6: 6}
+
+example_5()
