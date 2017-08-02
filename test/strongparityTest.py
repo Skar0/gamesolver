d816ca603bb5e18996de29340d98f0cfb025ea4f
clement <clement.tamines@gmail.com>
2017-08-03 00:57:18 +0200

    Added more tests

--- test/strongparityTest.py
+++ test/strongparityTest.py
@@ -1,6 +1,6 @@
 from tools import fileHandler as io
 from solvers import strongParity as sp
-
+from subprocess import call
 def fig47():
     fig47_graph = io.load_from_file("../assets/strong parity/fig47.txt")
     (a,b),(c,d) = sp.strongparity_solver2(fig47_graph)
@@ -44,3 +44,17 @@ def example_5():
     print a == [2, 1, 5] and b =={1: 2, 2: 2, 5: 5}and c ==[7, 6, 3, 4] and d == {3: 6, 4: 3, 6: 6}
 
 example_5()
+
+def recursive_ladder_1():
+    g = io.load_from_file("../assets/strong parity/recursive_ladder_1.txt")
+    (a,b),(c,d) = sp.strongparity_solver2(g)
+    print a == [1, 3, 4, 2, 0] and b == {1: 2, 3: 1} and c == [] and d == {}
+
+recursive_ladder_1()
+
+def recursive_ladder_2():
+    g = io.load_from_file("../assets/strong parity/recursive_ladder_2.txt")
+    (a,b),(c,d) = sp.strongparity_solver2(g)
+    print a == [] and b == {} and c == [6, 8, 9, 7, 5, 4, 0, 2, 1, 3] and d == {0: 4, 2: 4, 4: 5, 6: 7, 8: 6}
+
+recursive_ladder_2()
