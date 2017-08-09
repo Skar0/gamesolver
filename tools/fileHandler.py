cc1969adefde72a602c70d2c64653b941c5b6462
clement <clement.tamines@gmail.com>
2017-08-09 18:07:28 +0200

    Modified the method that writes the solution in dot format to take a solution as parameter. Added documentation.

--- tools/fileHandler.py
+++ tools/fileHandler.py
@@ -2,6 +2,12 @@ from graph import Graph
 
 
 def load_from_file(path):
+    """
+    Loads a game graph from a file specified by the path.
+    The file must be in PGSolver format.
+    :param path: path to the file.
+    :return: a Graph g corresponding to the game graph in the file.
+    """
     with open(path, 'r') as f:
         g = Graph()
         next(f)
@@ -22,11 +28,26 @@ def load_from_file(path):
     return g
 
 
-def write_solution_to_file(g, path):
+def write_solution_to_file(g, solution, player, path):
+    """
+    Writes the solution of a game in dot format to a file specified by the path.
+    Winning region and strategy of player 0 (1) is in blue (green).
+    Nodes belonging to player 0 (1) are circles (squares).
+    :param g: the game Graph.
+    :param solution: if player is 0, expected solution format is (W_0, sigma_0),(W_1, sigma_1). If player is 1, invert.
+    :param player: the player to which the first tuple in the solution belongs.
+    :param path: the path to the file in which we write the solution.
+    """
+
+    if player == 0:
+        (W_0, sigma_0), (W_1, sigma_1) = solution
+    else :
+        (W_1, sigma_1), (W_0, sigma_0) = solution
+
     with open(path, 'w') as f:
         f.write("digraph G {\n")
         f.write("splines=true;\nsep=\"+10,10\";\noverlap=scale;\nnodesep=0.6;\n")
-        for node in g.get_nodes():
+        for node in W_0:
             to_write = str(node) + "[label=\"" + str(node) + " " + str(g.get_node_priority(node)) + "\""
             if g.get_node_player(node) == 0:
                 to_write += ",shape=circle"
@@ -36,27 +57,42 @@ def write_solution_to_file(g, path):
                 pass
                 # error
 
-            if g.get_node_region(node) == 0:
-                to_write += ",color=blue3"
-            elif g.get_node_region(node) == 1:
-                to_write += ",color=forestgreen"
-            else:
-                pass
-                # error
-
+            to_write += ",color=blue3"
             to_write += "];\n"
+            f.write(to_write)
 
             for succ in g.successors[node]:
                 to_write += str(node) + " -> " + str(succ)
 
-                if succ == g.get_node_strategy(node):
-                    if g.get_node_player(node) == 0:
-                        to_write += '[color=blue3]\n'
-                    elif g.get_node_player(node) == 1:
-                        to_write += '[color=forestgreen]\n'
-                    else:
-                        pass
-                        # error
+                if succ == sigma_0[node]:
+                    to_write += '[color=blue3];\n'
+                elif succ == sigma_0[node]:
+                    to_write += '[color=forestgreen];\n'
+                else:
+                    to_write += ";\n"
+            f.write(to_write)
+
+        for node in W_1:
+            to_write = str(node) + "[label=\"" + str(node) + " " + str(g.get_node_priority(node)) + "\""
+            if g.get_node_player(node) == 0:
+                to_write += ",shape=circle"
+            elif g.get_node_player(node) == 1:
+                to_write += ",shape=square"
+            else:
+                pass
+                # error
+
+            to_write += ",color=forestgreen"
+            to_write += "];\n"
+            f.write(to_write)
+
+            for succ in g.successors[node]:
+                to_write += str(node) + " -> " + str(succ)
+
+                if succ == sigma_1[node]:
+                    to_write += '[color=forestgreen];\n'
+                elif succ == sigma_0[node]:
+                    to_write += '[color=blue3];\n'
                 else:
                     to_write += ";\n"
             f.write(to_write)
@@ -65,6 +101,12 @@ def write_solution_to_file(g, path):
 
 
 def write_graph_to_file(g, path):
+    """
+    Writes a game graph to a file specified by the path in dot format.
+    :param g: a game Graph.
+    :param path: the file to which we write the graph.
+    """
+
     with open(path, 'w') as f:
         f.write("digraph G {\n")
         for node in g.get_nodes():
