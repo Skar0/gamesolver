cd8249f297d4d119f9ffcbf7164f25dd7d3b5158
clement <clement.tamines@gmail.com>
2017-08-12 20:37:34 +0200

    Changed the doc and added some new arguments for benchmarking

--- solver.py
+++ solver.py
@@ -2,9 +2,11 @@
 import argparse
 from subprocess import call
 from benchmarks import reachability_benchmark as r_bench
+from benchmarks import strongParity_benchmark as sp_bench
 from solvers import reachability, weakParity, strongParity
 from tools import fileHandler as tools
 from tools import generators
+from tools import operations as ops
 
 
 def command_line_handler():
@@ -27,29 +29,18 @@ def command_line_handler():
 
     # create the parser for the "solve" command
     parser_solve = subparsers.add_parser('solve', help='Solve a game')
-    parser_solve.add_argument('-i', required=True, type=str, action='store', dest='inputFile', help='Input file')
-    parser_solve.add_argument('-o', required=False, type=str, action='store', dest='outputFile', help='Output file')
+    parser_solve.add_argument('-i', required=True, type=str, action='store', dest='inputFile', help='Input path for the arena file')
+    parser_solve.add_argument('-o', required=False, type=str, action='store', dest='outputFile', help='Output path for the solution')
 
     # create the parser for the "benchmark" command
     parser_benchmark = subparsers.add_parser('bench', help='Benchmark selected algorithm')
-    parser_benchmark.add_argument('-iter', required=True, type=int, action='store', dest='n',
+    parser_benchmark.add_argument('-n', required=True, type=int, action='store', dest='n',
                                   help='Number of iterations')
+    parser_benchmark.add_argument('-step', required=True, type=int, action='store', dest='s',
+                                  help='Steps taken by the benchmarker (1 = test all sizes from 1 to iter)')
+    parser_benchmark.add_argument('-plot', required=False, type=str, action='store', dest='outputPlot', help='Output path for the plot')
 
     return parser.parse_args()
-
-
-def get_solver(args):
-    """
-    Determines the solving function to be used by the program depending on the game type
-    :param args: the Namespace given by the parser
-    :return: the corresponding solving function
-    """
-    if args.target is not None:
-        return reachability.reachability_solver
-    elif args.wp:
-        return None  # placeholder for weak parity solver
-    else:
-        return None  # placeholder for strong parity solver
 
 
 def solver():
@@ -60,44 +51,46 @@ def solver():
     # Parsing the command line arguments
     args = command_line_handler()
 
-    # Solving mode
     if args.mode == "solve":
-        g = tools.load_from_file(args.inputFile)  # loading game
+        """ ----- Solving mode ----- """
+        g = tools.load_from_file(args.inputFile)  # loading game from the input file
+        player = 0 # default player is 0, so solution comes as (W_0,sigma_0), (W_1,sigma_1)
 
-        # Reachability
+        # Reachability (target and player is set)
         if args.target is not None:
             player = int(args.target[0])  # getting player (as int)
             target = map(int, args.target[1].split(","))  # getting targets (transforming them into ints)
-            solution = reachability.reachability_solver(g, target,
+            solution = reachability.reachability_solver_tuples(g, target,
                                                         player)  # calling reachability solver on the game
+            ops.print_solution(solution,player)
+
 
         # Weak parity
         elif args.wp:
-            solution = weakParity.weakParity_solver(g)  # calling weak parity solver on the game
+            solution = weakParity.weak_parity_solver(g)  # calling weak parity solver on the game
+            ops.print_solution(solution,player)
+
+        # Strong parity
         else:
-            solution = strongParity.strongparity_solver(g)  # calling weak parity solver on the game
+            solution = strongParity.strongparity_solver2(g)  # calling weak parity solver on the game
+            ops.print_solution(solution, player)
 
         # If output option chosen
         if args.outputFile is not None:
-            # TODO this needs to be changed to a writing function that takes a solution as argument
-            g.regions = solution[0]
-            g.strategies = solution[1]
-            tools.write_solution_to_file(g, args.outputFile + "_sol.dot")
-            # TODO remove : generated for testing
-            call(["neato", "-Tpdf", args.outputFile + "_sol.dot", "-o", args.outputFile + "_sol.pdf"])
+            tools.write_solution_to_file(g, solution, player, args.outputFile)
 
-    # Benchmark mode
     elif args.mode == "bench":
+        """ ----- Benchmark mode ----- """
         iter = args.n
+        isPlot = args.outputPlot is not None
 
         # Reachability
         if args.target is not None:
-            output_array = u"Générateur".center(30)+"|"+u"Noeuds (n)".center(12)+"|"+"Arcs (m)".center(10)+"|"\
+            print u"Générateur".center(30)+"|"+u"Noeuds (n)".center(12)+"|"+"Arcs (m)".center(10)+"|"\
                        +u"Atteignabilité (joueur 1)".center(28)+"|"+u"Atteignabilité (joueur 2)".center(28)+"\n"+\
                            "-"*108+"\n"
-            output_array+= r_bench.benchmark_complete_graph(iter,[1],plot=True)
-            output_array += r_bench.benchmark_worstcase_graph(iter,[1],plot=True)
-            print output_array
+            r_bench.benchmark(iter, generators.complete_graph2, [1], 1, iterations=3, plot=True,order=1,regression=True, step=100)
+
         # Weak parity
         elif args.wp:
             pass  # bench wp
