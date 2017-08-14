a104aa4015643d24959677f652e6e77fd55ff809
clement <clement.tamines@gmail.com>
2017-08-14 21:39:37 +0200

    Renamed test file. Added a general test launcher. Renamed an example file.

---
+++ test/reachability_test.py
@@ -0,0 +1,33 @@
+from tools import fileHandler as io
+from solvers import reachability as rs
+
+"""
+Test class for reachability games.
+Some examples are solved by our algorithm and we verify the solution. 
+"""
+
+
+def figure32():
+    """
+    Solves the reachability game from figure 3.2.
+    """
+    fig32_graph = io.load_from_file("assets/reachability/figure32.txt")
+    (W0, sig0), (W1, sig1) = rs.reachability_solver_tuples(fig32_graph, [1], 0)
+    return W0 == [1, 2, 3, 5] and sig0 == {1: 1, 2: 1, 5: 2} and W1 == [4, 6] and sig1 == {4: 6, 6: 4}
+
+
+def example_1():
+    """
+    Solves a simple example.
+    """
+    fig51_graph = io.load_from_file("assets/reachability/fig51.txt")
+    (W1, sig1), (W0, sig0) = rs.reachability_solver_tuples(fig51_graph, [8], 1)
+    return W1 == [8, 7, 4] and sig1 == {8: 3, 7: 8} and W0 == [1, 2, 3, 5, 6] and sig0 == {1: 5, 3: 3, 6: 5}
+
+
+def launch_tests():
+    """
+    Launches all tests.
+    :return: true if all tests succeeded.
+    """
+    return figure32() and example_1()
