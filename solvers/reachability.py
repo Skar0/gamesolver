f22fcbe69eaac3a5f4f9bde1708a539731c9f24b
clement <clement.tamines@gmail.com>
2018-04-19 18:02:35 +0200

    Added attractor computation algorithm (only yields the attractor with no strategies)

--- solvers/reachability.py
+++ solvers/reachability.py
@@ -170,3 +170,49 @@ def reachability_solver_non_removed(g, U, j, removed):
                             strat_opponent[node] = successor
 
     return (region_j, strat_j), (region_opponent, strat_opponent)
+
+
+def attractor(g, U, j):
+    """
+    Computes the attractor for player j of the set U in g. Does not create any strategy and only returns the set that
+    corresponds to the attractor.
+    :param g: the game graph.
+    :param U: the target set.
+    :param j: the player for which we compute the attractor.
+    :return: W the set of nodes corresponding to the attractor.
+    """
+    out = init_out(g)  # init out
+    queue = deque()  # init queue (deque is part of standard library and allows O(1) append() and pop() at either end)
+    # this dictionary is used to know if a node belongs to a winning region without
+    # iterating over both winning regions lists (we can check in O(1) in average)
+    regions = defaultdict(lambda: -1)
+    W = []  # the attractor
+    opponent = op.opponent(j)  # player j's opponent
+
+    # for each node in the target set U
+    for node in U:
+        queue.append(node)  # add node to the end of the queue
+        regions[node] = j  # set its regions to j (node is winning for j because reachability objective is satisfied)
+        W.append(node)  # add the node to the winning region list of j
+
+    # while queue is not empty
+    while queue:
+        s = queue.popleft()  # remove and return node on the left side of the queue (first in, first out)
+
+        # iterating over the predecessors of node s
+        for sbis in g.get_predecessors(s):
+            if regions[sbis] == -1:  # if sbis is not yet visited, its region is -1 by default
+                if g.get_node_player(sbis) == j:
+                    # belongs to j, set regions and strategy accordingly
+                    queue.append(sbis)
+                    regions[sbis] = j
+                    W.append(sbis)
+
+                elif g.get_node_player(sbis) == opponent:
+                    # belongs to j bar, decrement out. If out is 0, set the region accordingly
+                    out[sbis] -= 1
+                    if out[sbis] == 0:
+                        queue.append(sbis)
+                        regions[sbis] = j
+                        W.append(sbis)
+    return W
