281a059d46dfd446e3b05b9ba52d3f991b6b9120
clement <clement.tamines@student.umons.ac.be>
2018-08-13 16:06:14 +0200

    Added benchmarks for the antichain-based algorithm

--- benchmarks/strongparity_benchmark.py
+++ benchmarks/strongparity_benchmark.py
@@ -2,7 +2,7 @@
 from random import randint
 import matplotlib.pyplot as plt
 
-from solvers.strongparity import strong_parity_solver
+from solvers.strongparity import strong_parity_solver, strong_parity_antichain_based
 from tools import timer, generators
 
 """
@@ -146,4 +146,138 @@ def benchmark_worst_case(n, iterations=3, step=10, plot=False, path=""):
         plt.clf()
         plt.close()
 
+def benchmark_random_antichain_based(n, iterations=3, step=10, plot=False, path=""):
+    """
+    This function is unfinished.
+    Benchmarks the recursive algorithm for strong parity games using a random generator. Calls strong parity solver
+    on games generated using the random generator function. Games of 1 to n are solved and a timer records the
+    time taken to get the solution.The solver can be timed several times and the minimum value is selected using
+    optional parameter iterations (to avoid recording time spikes and delays due to system load). The result can be
+    plotted using matplotlib.
+    :param n: number of nodes in generated graph.
+    :param iterations: number of times the algorithm is timed (default is 3).
+    :param step: step to be taken in the generation.
+    :param plot: if True, plots the data using matplotlib.
+    :param path: path to the file in which to write the result.
+    """
+
+    y = []  # list for the time recordings
+    n_ = []  # list for the x values
+
+    total_time = 0  # accumulator to record total time
+
+    nbr_generated = 0  # conserving the number of generated mesures (used to get the index of a mesure)
+
+    chrono = timer.Timer(verbose=False)  # Timer object
+
+    info = "Time to solve"  # info about the current benchmark
+
+    # print first line of output
+    print u"Generator".center(40) + "|" + u"Nodes (n)".center(12) + "|" + info.center(40) + "\n" + \
+          "-" * 108
+
+    # games generated are size 1 to n
+    for i in range(10, n + 1, step):
+        temp = []  # temp list for #iterations recordings
+        g = generators.random(i, i, 1, (i / 2) )# generated game
+        # #iterations calls to the solver are timed
+        for j in range(iterations):
+            with chrono:
+                strong_parity_antichain_based(g,0)  # solver call
+            temp.append(chrono.interval)  # add time recording
+
+        min_recording = min(temp)
+        y.append(min_recording)  # get the minimum out of #iterations recordings
+        n_.append(i)
+        total_time += min_recording
+
+        print "Random graph".center(40) + "|" + str(i).center(12) + "|" \
+              + str(y[nbr_generated]).center(40) + "\n" + "-" * 108
+
+        nbr_generated += 1  # updating the number of generated mesures
+
+        # at the end, print total time
+    print "-" * 108 + "\n" + "Total time".center(40) + "|" + "#".center(12) + "|" + \
+          str(total_time).center(40) + "\n" + "-" * 108 + "\n"
+
+    if plot:
+        plt.grid(True)
+        plt.title(u"Random graphs of size 1 à " + str(n))
+        plt.xlabel(u'number of nodes')
+        plt.ylabel(u'time (s)')
+        points, = plt.plot(n_, y, 'g.', label=u"Execution time")
+        plt.legend(loc='upper left', handles=[points])
+        plt.savefig(path, bbox_inches='tight')
+        plt.clf()
+        plt.close()
+
+
+def benchmark_worst_case_antichain_based(n, iterations=3, step=10, plot=False, path=""):
+    """
+    Benchmarks the recursive algorithm for strong parity games using the worst case generator which yields an
+    exponential complexity. Calls strong parity solver on games generated using the worst case generator function.
+    Games of size 5 to 5*n are solved and a timer records the time taken to get the solution.The solver can be timed
+    several times and the minimum value is selected using optional parameter iterations (to avoid recording time
+    spikes and delays due to system load). The result can be plotted using matplotlib.
+    :param n: number of nodes in generated graph (nodes is 5*n due to construction).
+    :param iterations: number of times the algorithm is timed (default is 3).
+    :param step: step to be taken in the generation.
+    :param plot: if True, plots the data using matplotlib.
+    :param path: path to the file in which to write the result.
+    """
+
+    y = []  # list for the time recordings
+    n_ = []  # list for the x values (5 to 5n)
+
+    total_time = 0  # accumulator to record total time
+
+    nbr_generated = 0  # conserving the number of generated mesures (used to get the index of a mesure)
+
+    chrono = timer.Timer(verbose=False)  # Timer object
+
+    info = "Time to solve (s)"  # info about the current benchmark
+
+    # print first line of output
+    print u"Generator".center(40) + "|" + u"Nodes (n)".center(12) + "|" + info.center(40) + "\n" + \
+          "-" * 108
+
+    # games generated are size 1 to n
+    for i in range(1, n + 1, step):
+        temp = []  # temp list for #iterations recordings
+        g = generators.strong_parity_worst_case(i)  # generated game
+
+        # #iterations calls to the solver are timed
+        for j in range(iterations):
+            with chrono:
+                strong_parity_antichain_based(g,1)  # solver call
+            temp.append(chrono.interval)  # add time recording
+
+        min_recording = min(temp)
+        y.append(min_recording)  # get the minimum out of #iterations recordings
+        n_.append(5 * i)
+        total_time += min_recording
+
+        print "Worst-case graph".center(40) + "|" + str(i * 5).center(12) + "|" \
+              + str(y[nbr_generated]).center(40) + "\n" + "-" * 108
+
+        nbr_generated += 1  # updating the number of generated mesures
+
+        # at the end, print total time
+    print "-" * 108 + "\n" + "Total (s)".center(40) + "|" + "#".center(12) + "|" + \
+          str(total_time).center(40) + "\n" + "-" * 108 + "\n"
+
+    if plot:
+        plt.grid(True)
+        plt.title(u"Worst-case graph of size 5 to " + str(5 * n))
+        plt.xlabel(u'number of nodes')
+        plt.ylabel(u'time (s)')
+        # plt.yscale("log") allows logatithmic y-axis
+        points, = plt.plot(n_, y, 'g.', label=u"Execution time")
+        plt.legend(loc='upper left', handles=[points])
+        plt.savefig(path, bbox_inches='tight')
+        plt.clf()
+        plt.close()
+
 # benchmark_worst_case(30, iterations=3, step=1, plot=True)
+#benchmark_worst_case_antichain_based(55, iterations=3, step=1, plot=True, path="antichain-based-55n-1s.pdf")
+#benchmark_random_antichain_based(350, iterations=3, step=10, plot=True, path="antichain-based-random-350n-10s.pdf")
