c53df85c75c6dae2d9fc33a51e56806ba02a8aa4
clement <clement.tamines@gmail.com>
2018-08-14 20:46:54 +0100

    Added a function to write a generalized parity game solution to a file. Added documentation.

--- tools/file_handler.py
+++ tools/file_handler.py
@@ -32,8 +32,8 @@ def load_from_file(path):
 
 def load_generalized_from_file(path):
     """
-    Loads a game graph from a file specified by the path.
-    The file must be in PGSolver format.
+    Loads a generalized parity game graph from a file specified by the path.
+    The file must be in PGSolver format for generalized parity.
     :param path: path to the file.
     :return: a Graph g corresponding to the game graph in the file.
     """
@@ -128,6 +128,59 @@ def write_solution_to_file(g, solution, player, path):
 
         f.write('}')
 
+def write_generalized_solution_to_file(g, W1, W2, path):
+    """
+    Writes the solution of a generalized parity game in dot format to a file specified by the path.
+    Winning region of player 0 (1) is in blue (green).
+    Nodes belonging to player 0 (1) are circles (squares).
+    :param g: the game Graph.
+    :param W1: winning region of player 0 (1).
+    :param W2: winning region of player 1 (2).
+    :param path: the path to the file in which we write the solution.
+    """
+
+    with open(path, 'w') as f:
+        f.write("digraph G {\n")
+        f.write("splines=true;\nsep=\"+10,10\";\noverlap=scale;\nnodesep=0.6;\n")
+        for node in W1:
+            to_write = str(node) + "[label=\"v" + str(node) + " " + str(g.nodes[node][1:]) + "\""
+            if g.get_node_player(node) == 0:
+                to_write += ",shape=circle"
+            elif g.get_node_player(node) == 1:
+                to_write += ",shape=square"
+            else:
+                pass
+                # error
+
+            to_write += ",color=blue3"
+            to_write += "];\n"
+            f.write(to_write)
+
+            for succ in g.successors[node]:
+                to_write += str(node) + " -> " + str(succ)
+                to_write += '[color=black];\n'
+            f.write(to_write)
+
+        for node in W2:
+            to_write = str(node) + "[label=\"v" + str(node) + " " + str(g.nodes[node][1:]) + "\""
+            if g.get_node_player(node) == 0:
+                to_write += ",shape=circle"
+            elif g.get_node_player(node) == 1:
+                to_write += ",shape=square"
+            else:
+                pass
+                # error
+
+            to_write += ",color=forestgreen"
+            to_write += "];\n"
+            f.write(to_write)
+
+            for succ in g.successors[node]:
+                to_write += str(node) + " -> " + str(succ)
+                to_write += '[color=black];\n'
+            f.write(to_write)
+
+        f.write('}')
 
 def write_graph_to_file(g, path):
     """
@@ -156,3 +209,10 @@ def write_graph_to_file(g, path):
             f.write(to_write)
 
         f.write('}')
+
+"""
+g = load_generalized_from_file("../assets/generalized parity/simple_example3.txt")
+W1 = [1,2,3]
+W2 = [4,5,6]
+write_generalized_solution_to_file(g,W1,W2,"test.dot")
+"""
