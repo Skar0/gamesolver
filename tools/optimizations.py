16836807098b1bbd9775fffeb7a37f51546bda30
clement <clement.tamines@gmail.com>
2018-08-14 20:57:28 +0100

    Added documentation.

--- tools/optimizations.py
+++ tools/optimizations.py
@@ -6,13 +6,12 @@ import copy
 
 def compress_priorities(g):
     """
-    Returns a.
-    :param j: the player (0 or 1).
-    :return: its opponent.
+    :param g: a game arena
+    :return: the game arena g in which the priorities have been compressed.
     """
     g_copy = copy.deepcopy(g)
     nodes = g_copy.get_nodes_descriptors()  # Nodes from g
-    priorities = sorted(nodes.iteritems(), key=lambda x:x[1][1])
+    priorities = sorted(nodes.iteritems(), key=lambda x:x[1][1]) # sorts nodes per priority
     current = priorities[0][1][1]
     if current % 2 == 0:
         start = 0
@@ -28,28 +27,3 @@ def compress_priorities(g):
             g_copy.nodes[elem[0]] = tuple([elem[1][0], val])
 
     return g_copy
-
-
-
-#import file_handler as io
-#g = io.load_from_file("/home/clement/PycharmProjects/gamesolver/assets/reachability/fig51.txt")
-
-#compress_priorities(g)
-
-""""
-    
-    # get all node indexes in node tuple (index, (node_player, node_priority)) when node_priority is i
-    return [k for k, v in nodes.iteritems() if v[j] == i]
-
-
-def max_priority(g):
-    
-    Returns the maximum priority occurring in game graph g.
-    :param g: a game graph.
-    :return: the maximum priority in g.
-    
-    nodes = g.nodes  # Nodes from g
-    # get maximum node tuple (index, (node_player, node_priority)) according to its priority, then select its priority
-    return max(nodes.iteritems(), key=lambda (k, v): v[1])[1][1]
-
-"""
