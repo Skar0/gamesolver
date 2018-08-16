06eecaa12447123ad05932b17cca1bc75340be59
clement <clement.tamines@gmail.com>
2018-08-16 22:14:13 +0100

    Added documentation

--- benchmarks/generalizedparity_benchmark.py
+++ benchmarks/generalizedparity_benchmark.py
@@ -6,18 +6,14 @@ from solvers.generalizedparity import generalized_parity_solver
 from tools import timer, generators
 
 """
-This module benchmarks the strong parity games.
+This module benchmarks the generalized parity games algorithm
 """
 
 
 def benchmark_random_k_functions(n, k, iterations=3, step=10, plot=False, path=""):
     """
-    This function is unfinished.
-    Benchmarks the recursive algorithm for strong parity games using a random generator. Calls strong parity solver
-    on games generated using the random generator function. Games of 1 to n are solved and a timer records the
-    time taken to get the solution.The solver can be timed several times and the minimum value is selected using
-    optional parameter iterations (to avoid recording time spikes and delays due to system load). The result can be
-    plotted using matplotlib.
+    Benchmarks the classical algorithm for generalized parity games using a random game arenas generator.
+    Arenas size 1 to n, each with k priority functions are solved.
     :param n: number of nodes in generated graph.
     :param k: number of priority functions in the generated graph.
     :param iterations: number of times the algorithm is timed (default is 3).
@@ -44,7 +40,6 @@ def benchmark_random_k_functions(n, k, iterations=3, step=10, plot=False, path="
     # games generated are size 1 to n
     for i in range(2, n + 1, step):
         g = generators.random_generalized(i, k, i, 1, (i / 2))
-        print(g)
         temp = []
         # #iterations calls to the solver are timed
         for j in range(iterations):
@@ -57,7 +52,7 @@ def benchmark_random_k_functions(n, k, iterations=3, step=10, plot=False, path="
         n_.append(i)
         total_time += min_recording
 
-        print "Random graph".center(40) + "|" + str(i).center(12) + "|" \
+        print "Random graphs".center(40) + "|" + str(i).center(12) + "|" \
               + str(y[nbr_generated]).center(40) + "\n" + "-" * 108
 
         nbr_generated += 1  # updating the number of generated mesures
@@ -79,12 +74,8 @@ def benchmark_random_k_functions(n, k, iterations=3, step=10, plot=False, path="
 
 def benchmark_random_n_nodes(n, k, iterations=3, step=10, plot=False, path=""):
     """
-    This function is unfinished.
-    Benchmarks the recursive algorithm for strong parity games using a random generator. Calls strong parity solver
-    on games generated using the random generator function. Games of 1 to n are solved and a timer records the
-    time taken to get the solution.The solver can be timed several times and the minimum value is selected using
-    optional parameter iterations (to avoid recording time spikes and delays due to system load). The result can be
-    plotted using matplotlib.
+    Benchmarks the classical algorithm for generalized parity games using a random game arenas generator.
+    Arenas of size n with 1 to k priority functions are solved.
     :param n: number of nodes in generated graph.
     :param k: number of priority functions in the generated graph.
     :param iterations: number of times the algorithm is timed (default is 3).
@@ -109,8 +100,8 @@ def benchmark_random_n_nodes(n, k, iterations=3, step=10, plot=False, path=""):
           "-" * 108
 
     # games generated are size 1 to n
-    for i in range(1, k + 1, step):
-        g = generators.random_generalized(100, i, 100, 1, 20)
+    for i in range(2, k + 1, step):
+        g = generators.random_generalized(n, i, n, 1, n/2)
         temp = []
         # #iterations calls to the solver are timed
         for j in range(iterations):
@@ -145,12 +136,8 @@ def benchmark_random_n_nodes(n, k, iterations=3, step=10, plot=False, path=""):
 
 def benchmark_worstcase_n_nodes(n, k, iterations=3, step=10, plot=False, path=""):
     """
-    This function is unfinished.
-    Benchmarks the recursive algorithm for strong parity games using a random generator. Calls strong parity solver
-    on games generated using the random generator function. Games of 1 to n are solved and a timer records the
-    time taken to get the solution.The solver can be timed several times and the minimum value is selected using
-    optional parameter iterations (to avoid recording time spikes and delays due to system load). The result can be
-    plotted using matplotlib.
+    Benchmarks the classical algorithm for generalized parity games using the worst case game arenas generator.
+    Arenas are the arenas of the worst case for the recursive algorithm for parity games, generalized with k functions
     :param n: number of nodes in generated graph.
     :param k: number of priority functions in the generated graph.
     :param iterations: number of times the algorithm is timed (default is 3).
@@ -175,7 +162,7 @@ def benchmark_worstcase_n_nodes(n, k, iterations=3, step=10, plot=False, path=""
           "-" * 108
 
     # games generated are size 1 to n
-    for i in range(1, 6, step):
+    for i in range(1, n, step):
         g = generators.strong_parity_worst_case(i)  # generated game
         g = generators.multiple_priorities(g,k)
 
@@ -191,7 +178,7 @@ def benchmark_worstcase_n_nodes(n, k, iterations=3, step=10, plot=False, path=""
         n_.append(i)
         total_time += min_recording
 
-        print "Random graph".center(40) + "|" + str(i).center(12) + "|" \
+        print "Worst-case graph".center(40) + "|" + str(i).center(12) + "|" \
               + str(y[nbr_generated]).center(40) + "\n" + "-" * 108
 
         nbr_generated += 1  # updating the number of generated mesures
@@ -202,7 +189,7 @@ def benchmark_worstcase_n_nodes(n, k, iterations=3, step=10, plot=False, path=""
 
     if plot:
         plt.grid(True)
-        plt.title(u"Random graph of size " + str(100)+" with 1 to "+str(k)+" priority functions")
+        plt.title(u"Worst-case graph of size 1 to " + str(n)+" with 1 to "+str(k)+" priority functions")
         plt.xlabel(u'number of nodes')
         plt.ylabel(u'time (s)')
         points, = plt.plot(n_, y, 'g.', label=u"Execution time")
@@ -211,6 +198,6 @@ def benchmark_worstcase_n_nodes(n, k, iterations=3, step=10, plot=False, path=""
         plt.clf()
         plt.close()
 
-benchmark_worstcase_n_nodes(100, 3, iterations=3, step=1, plot=True, path="generalized-worst-up100n-3k-10s.pdf")
+#benchmark_worstcase_n_nodes(100, 3, iterations=3, step=1, plot=True, path="generalized-worst-up100n-3k-10s.pdf")
 
-benchmark_random_k_functions(200, 3, iterations=3, step=10, plot=True, path="generalized-random-up100n-3k-10s.pdf")
+#benchmark_random_k_functions(200, 3, iterations=3, step=10, plot=True, path="generalized-random-up100n-3k-10s.pdf")
